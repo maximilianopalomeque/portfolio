7d75b69f3a297bff3b97de23ce72eb23e12d2159
Maximiliano Palomeque <maximilianopalomeque@gmail.com>
2024-04-20 19:14:59 -0300

    contact buttons

--- src/components/LandingSections/Contact.tsx
+++ src/components/LandingSections/Contact.tsx
@@ -1,13 +1,33 @@
 import LayoutContainer from "../LayoutContainer";
+import handshakeImg from "../../../public/img/handshake.png";
+
+import { MdOutlineEmail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <div className="pt-20 pb-10">
       <LayoutContainer>
-        <div className="flex justify-center">
-          <h2 className="text-center text-4xl font-bold max-w-md">
+        <div className="flex flex-col items-center">
+          <div className="w-16 h-16 rounded-full bg-accent flex justify-center items-center p-3">
+            <img src={handshakeImg.src} alt="handshake icon" />
+          </div>
+          <h2 className="text-center text-4xl font-bold max-w-md mt-4">
             Contame sobre tu proximo proyecto!
           </h2>
+          <div className="flex gap-2 mt-6">
+            <a href="mailto:maximilianopalomeque@gmail.com">
+              <button className="btn btn-outline">
+                <MdOutlineEmail /> Email
+              </button>
+            </a>
+            <a href="https://wa.me/542215739485" target="_blank">
+              <button className="btn btn-accent">
+                <FaWhatsapp />
+                WhatsApp
+              </button>
+            </a>
+          </div>
         </div>
       </LayoutContainer>
     </div>
