0af87ad21a950b61274829fdd9653b7576b21167
Maximiliano Palomeque <maximilianopalomeque@gmail.com>
2024-06-03 14:17:13 -0300

    header animation, show scroll animation

--- src/components/LandingSections/Header.tsx
+++ src/components/LandingSections/Header.tsx
@@ -3,19 +3,21 @@ import LayoutContainer from "../LayoutContainer";
 export default function Header() {
   return (
     <LayoutContainer>
-      <div className="h-[100vh] flex flex-col justify-center">
-        <h2 className="text-center xl:text-left text-3xl xl:text-6xl xl:max-w-3xl">
-          Transformando <span className="font-semibold">tu visión</span> en
-          <span className="font-semibold"> experiencias web</span>.
+      <div className="h-[100vh] flex flex-col justify-center items-center">
+        <h2 className="text-gray-600 mb-1 text-center text-sm scale-in-ver-bottom">
+          DESARROLLADOR WEB FULL STACK
         </h2>
-        <h2 className="text-gray-600 mt-2 text-center xl:text-left">
-          Desarrollador full stack, elevando tus proyectos digitales.
-        </h2>
-        <div className="mt-8">
-          <div className="flex gap-3 justify-center xl:justify-start">
-            <button className="btn btn-secondary">Trabajos</button>
-            <button className="btn btn-accent">Contacto</button>
+        <h2 className="text-center text-5xl xl:text-9xl">
+          <div className="scale-in-ver-bottom">Transformando</div>
+          <div className="font-semibold scale-in-ver-bottom">
+            tu visión <span className="font-normal">en</span>
           </div>
+          <div className="font-semibold scale-in-ver-bottom">
+            experiencias web
+          </div>
+        </h2>
+        <div className="scroll-indicator absolute bottom-5">
+          <div className="scroll-dot"></div>
         </div>
       </div>
     </LayoutContainer>
