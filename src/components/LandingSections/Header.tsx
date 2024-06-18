d5888fc42c79f6c27aa803f5cff5c485fd8b4faa
Maximiliano Palomeque <maximilianopalomeque@gmail.com>
2024-06-18 15:07:06 -0300

    fix main title

--- src/components/LandingSections/Header.tsx
+++ src/components/LandingSections/Header.tsx
@@ -1,7 +1,8 @@
 import LayoutContainer from "../LayoutContainer";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function Header() {
+  const locale = useLocale();
   const translation = useTranslations("landing");
   const titleDev = translation("header.title-dev");
   const title1 = translation("header.title-1");
@@ -15,12 +16,28 @@ export default function Header() {
         <h2 className="text-gray-600 mb-1 text-center text-sm scale-in-ver-bottom">
           {titleDev}
         </h2>
-        <h2 className="text-center text-5xl xl:text-9xl">
+        <h2 className={`text-center text-5xl xl:text-9xl`}>
           <div className="scale-in-ver-bottom">{title1}</div>
           <div className="font-semibold scale-in-ver-bottom">
-            {title2} <span className="font-normal">{title3}</span>
+            {title2}{" "}
+            <span
+              className={`${
+                locale === "en" ? "hidden" : "visible"
+              } font-normal`}
+            >
+              {title3}
+            </span>
           </div>
-          <div className="font-semibold scale-in-ver-bottom">{title4}</div>
+          <div className="font-semibold scale-in-ver-bottom">
+            <span
+              className={`${
+                locale === "es" ? "hidden" : "visible"
+              } font-normal`}
+            >
+              {title3}
+            </span>{" "}
+            {title4}
+          </div>
         </h2>
         <div className="scroll-indicator absolute bottom-5">
           <div className="scroll-dot"></div>
